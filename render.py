52ae946d46b0151b55436df8fff281cf8486d625
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2018-12-02 16:52:43 +0100

    Take it slowly with Modernity

--- render.py
+++ render.py
@@ -160,18 +160,18 @@ template = env.from_string(
 
 
     // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
     // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
 
     // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
+    $navbarBurgers.forEach( function(el) {
+      el.addEventListener('click', function() {
 
         // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
+        var target = el.dataset.target;
+        var $target = document.getElementById(target);
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle('is-active')
@@ -180,10 +180,10 @@ template = env.from_string(
     });
     }
 
-    const $rest = document.getElementById("navbar-rest")
-    $rest.addEventListener("click", () => {
+    var $rest = document.getElementById("navbar-rest")
+    $rest.addEventListener("click", function() {
         $rest.classList.remove('is-active')
-        $navbarBurgers.forEach(el => {
+        $navbarBurgers.forEach(function(el) {
           el.classList.toggle("is-active")
         })
     })
